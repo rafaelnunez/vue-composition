8859feccd242bd1687d39fa576608137cc814933
Rafael Nunez Manotas <rafael.nunez@ingeniousio.com>
2024-01-26 15:31:50 -0500

    feat: implemented reactive vue feature.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,58 +1,37 @@
 <template>
   <div class="home">
+
+    <h1>{{ header }}</h1>
+
+    <h3>{{ counterData.title }}:</h3>
+
     <div>
       <button @click="decreaseCounter" class="btn">-</button>
-      <span class="counter">{{ counter }}</span>
+      <span class="counter">{{ counterData.count }}</span>
       <button @click="increaseCounter" class="btn">+</button>
     </div>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text">
+    </div>
+
   </div>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
+  import { reactive } from 'vue'
 
-  const counter = ref(0)
+  const header = 'This is my amazing counter'
 
-  const increaseCounter = () => counter.value++
-  const decreaseCounter = () => counter.value--
+  const counterData = reactive({
+    count: 0,
+    title: "My counter"
+  })
+
+  const increaseCounter = () => counterData.count++
+  const decreaseCounter = () => counterData.count--
 </script>
-
-<!--
-<script>
-  import { ref } from 'vue'
-
-  export default {
-    setup() {
-      const counter = ref(0)
-
-      const increaseCounter = () => counter.value++
-      const decreaseCounter = () => counter.value--
-
-      return {
-        counter,
-        increaseCounter,
-        decreaseCounter,
-      }
-    }
-  }
-</script>-->
-<!--<script>
-export default {
-  data() {
-    return {
-      counter: 0
-    }
-  },
-  methods: {
-    increaseCounter() {
-      this.counter++
-    },
-    decreaseCounter() {
-      this.counter--
-    },
-  }
-}
-</script>-->
 
 <style>
 .home {
@@ -62,5 +41,8 @@ export default {
 .btn, .counter {
   font-size: 40px;
   margin: 10px;
+}
+.edit {
+  margin-top: 10px;
 }
 </style>
