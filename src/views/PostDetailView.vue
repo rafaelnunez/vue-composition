088a56f665b0421b46ea850c7570f2d1da054e94
Rafael Nunez Manotas <rafael.nunez@ingeniousio.com>
2024-01-30 17:15:48 -0500

    feat: implementing  the useRoute feature.

---
+++ src/views/PostDetailView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="post-detail">
+    <h1>This is a post page</h1>
+    <p>Display the content of post with ID of {{ $route.params.id }} here!</p>
+    <div>
+      <button @click="showPostId">Show POst ID</button>
+    </div>
+    <div>
+      <button @click="goHomeIn3Seconds">Go to Home in 3 seconds</button>
+    </div>
+    <div>
+      <button @click="goTofirstPost">Go to First post</button>
+    </div>
+    <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+
+/*
+  imports
+*/
+const route = useRoute();
+const router = useRouter();
+
+const showPostId = () => {
+  alert(`The ID of this post is: ${route.params.id}`);
+};
+
+const goHomeIn3Seconds = () => {
+  setTimeout(() => {
+    router.push({ name: "home" });
+  }, 3000);
+};
+const goTofirstPost = () => {
+  router.push({ name: "postDetail", params: { id: "id1" } });
+};
+</script>
